0228e559f1b3ecb8321317b871675efa322fa79b
HuniHuns <i690367@gmail.com>
2025-04-30 11:25:49 +0900

    project init

---
+++ src/test/java/samsamohoh/ai_mockly/AiMocklyApplicationTests.java
@@ -0,0 +1,13 @@
+package samsamohoh.ai_mockly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMocklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
