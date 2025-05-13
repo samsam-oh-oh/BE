b75a94f3ade02536d78095dec2cf9eae087825b9
HuniHuns <i690367@gmail.com>
2025-05-13 11:31:12 +0900

    project setting

---
+++ src/main/java/samsamoo/ai_mockly/AiMocklyApplication.java
@@ -0,0 +1,13 @@
+package samsamoo.ai_mockly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMocklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMocklyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/samsamoo/ai_mockly/AiMocklyApplicationTests.java
@@ -0,0 +1,13 @@
+package samsamoo.ai_mockly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMocklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
