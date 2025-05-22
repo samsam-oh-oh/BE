735aac227e85f712f3b7d07e58cc2311393a8f80
HuniHuns <i690367@gmail.com>
2025-05-22 17:14:13 +0900

    [FEAT] 카카오 로그인 api

--- src/main/java/samsamoo/ai_mockly/domain/auth/application/AuthService.java
+++ src/main/java/samsamoo/ai_mockly/domain/auth/application/AuthService.java
@@ -1,18 +1,71 @@
 package samsamoo.ai_mockly.domain.auth.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import samsamoo.ai_mockly.domain.auth.dto.request.LoginReq;
 import samsamoo.ai_mockly.domain.auth.dto.response.DuplicateCheckRes;
+import samsamoo.ai_mockly.domain.auth.dto.response.LoginRes;
+import samsamoo.ai_mockly.domain.member.domain.Member;
 import samsamoo.ai_mockly.domain.member.domain.repository.MemberRepository;
+import samsamoo.ai_mockly.domain.member.dto.response.MemberDTO;
 import samsamoo.ai_mockly.global.common.SuccessResponse;
+import samsamoo.ai_mockly.global.security.jwt.JwtTokenProvider;
+import samsamoo.ai_mockly.infrastructure.redis.RedisUtil;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class AuthService {
 
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshExpiration;
+
+    private static String RT_PREFIX = "RT_";
+    private static String BL_AT_PREFIX = "BL_AT_";
+
+    private final KakaoTokenValidator kakaoTokenValidator;
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisUtil redisUtil;
+
+    @Transactional
+    public SuccessResponse<LoginRes> signUpOrLogin(LoginReq loginReq) {
+        String idToken = loginReq.getIdToken();
+        MemberDTO memberDTO = kakaoTokenValidator.validateAndExtract(idToken);
+
+        Member member = memberRepository.findByKakaoId(memberDTO.getKakaoId())
+                .orElseGet(() -> registerNewMember(memberDTO));
+
+        boolean isNewMember = member.getNickname().startsWith("|KAKAO");
+
+        String accessToken = jwtTokenProvider.createAccessToken(member);
+        String refreshToken = jwtTokenProvider.createRefreshToken();
+
+        redisUtil.setDataExpire(RT_PREFIX + refreshToken, member.getNickname(), refreshExpiration);
+
+        LoginRes loginRes = LoginRes.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .isNewMember(isNewMember)
+                .build();
+
+        return SuccessResponse.of(loginRes);
+    }
+
+    private Member registerNewMember(MemberDTO memberDTO) {
+        Member member = Member.builder()
+                .kakaoId(memberDTO.getKakaoId())
+                .nickname("|KAKAO" + (int)(Math.random() * 9000 + 1000))
+                .profileImage(null)
+                .maxScore(0)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
 
     public SuccessResponse<DuplicateCheckRes> checkNicknameDuplicate(String nickname) {
         if(nickname == null || nickname.isBlank()) {
