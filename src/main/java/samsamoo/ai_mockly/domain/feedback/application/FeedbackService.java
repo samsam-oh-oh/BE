bca9acbe4c70bbafb0df702d1341338577315394
HuniHuns <i690367@gmail.com>
2025-05-26 22:02:55 +0900

    [FEAT] 피드백 내용 리스트 호출 api

--- src/main/java/samsamoo/ai_mockly/domain/feedback/application/FeedbackService.java
+++ src/main/java/samsamoo/ai_mockly/domain/feedback/application/FeedbackService.java
@@ -7,10 +7,14 @@ import org.springframework.transaction.annotation.Transactional;
 import samsamoo.ai_mockly.domain.feedback.domain.Feedback;
 import samsamoo.ai_mockly.domain.feedback.domain.repository.FeedbackRepository;
 import samsamoo.ai_mockly.domain.feedback.dto.request.FeedbackSaveReq;
+import samsamoo.ai_mockly.domain.feedback.dto.response.FeedbackContentsRes;
 import samsamoo.ai_mockly.domain.member.domain.Member;
 import samsamoo.ai_mockly.domain.member.domain.repository.MemberRepository;
 import samsamoo.ai_mockly.global.common.Message;
 import samsamoo.ai_mockly.global.common.SuccessResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -40,5 +44,21 @@ public class FeedbackService {
                 .message("면접 결과 리포트 저장이 완료되었습니다.")
                 .build();
         return SuccessResponse.of(message);
+    }
+
+    public SuccessResponse<List<FeedbackContentsRes>> getFeedbackContents(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BadCredentialsException("해당 아이디를 갖는 유저가 없습니다."));
+
+        List<Feedback> feedbackList = feedbackRepository.findAllByMember(member);
+
+        List<FeedbackContentsRes> feedbackContentList = feedbackList.stream()
+                .map(feedback -> FeedbackContentsRes.builder()
+                        .id(feedback.getId())
+                        .content(feedback.getContent())
+                        .build())
+                .collect(Collectors.toList());
+
+        return SuccessResponse.of(feedbackContentList);
     }
 }
--- src/main/java/samsamoo/ai_mockly/domain/feedback/presentation/FeedbackApi.java
+++ src/main/java/samsamoo/ai_mockly/domain/feedback/presentation/FeedbackApi.java
@@ -9,14 +9,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import samsamoo.ai_mockly.domain.feedback.dto.request.FeedbackSaveReq;
+import samsamoo.ai_mockly.domain.feedback.dto.response.FeedbackContentsRes;
 import samsamoo.ai_mockly.domain.member.domain.Member;
 import samsamoo.ai_mockly.global.annotation.LoginMember;
 import samsamoo.ai_mockly.global.common.Message;
 import samsamoo.ai_mockly.global.common.SuccessResponse;
 import samsamoo.ai_mockly.global.exception.ErrorResponse;
+
+import java.util.List;
 
 @Tag(name = "FeedbackAPI", description = "면접 피드백 및 결과 리포트 관련 API입니다.")
 public interface FeedbackApi {
@@ -34,6 +38,21 @@ public interface FeedbackApi {
     })
     @PostMapping("/save")
     ResponseEntity<SuccessResponse<Message>> saveFeedback(
-            @Parameter(description = "정보를 불러오고 싶은 회원의 Access Token을 입력하시오.", required = true) @Valid @LoginMember Member member,
-            @Parameter(description = "Schemas의 ReedbackSaveReq를 참고", required = true) @RequestBody FeedbackSaveReq feedbackSaveReq);
+            @Parameter(description = "정보를 불러오고 싶은 회원의 Access Token을 입력하시오.", required = true) @LoginMember Member member,
+            @Parameter(description = "Schemas의 ReedbackSaveReq를 참고", required = true) @Valid @RequestBody FeedbackSaveReq feedbackSaveReq);
+
+    @Operation(summary = "피드백 내용 리스트 불러오기", description = "회원의 피드백 내용을 가져옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "회원의 피드백 내용 불러오기 성공",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = FeedbackContentsRes.class))}
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "회원의 피드백 내용 불러오기 실패(잘못된 요청 방식)",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}
+            )
+    })
+    @GetMapping("/contents/all")
+    ResponseEntity<SuccessResponse<List<FeedbackContentsRes>>> getFeedbackContents(
+            @Parameter(description = "피드백 내용을 불러오고 싶은 회원의 Access Token을 입력하시오.", required = true) @LoginMember Member member);
 }
