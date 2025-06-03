852f07686c015b46328796315e5ae707233ce018
HuniHuns <i690367@gmail.com>
2025-06-04 00:20:33 +0900

    [BUG] Config WhiteList 수정

--- src/main/java/samsamoo/ai_mockly/global/config/SecurityConfig.java
+++ src/main/java/samsamoo/ai_mockly/global/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
             "/scores/**",
             "/feedbacks/**",
             "/llm/**",
-            "/point/**"
+            "/points/**"
     };
 
     @Bean
