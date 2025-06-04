85ba1de78f67e4b8e21e0ab0c844525dd906d44d
HuniHuns <i690367@gmail.com>
2025-06-04 14:09:09 +0900

    [REFACTOR] rankingList 중 feedback 조회 범위 수정

--- src/main/java/samsamoo/ai_mockly/domain/feedback/domain/repository/FeedbackRepository.java
+++ src/main/java/samsamoo/ai_mockly/domain/feedback/domain/repository/FeedbackRepository.java
@@ -1,6 +1,8 @@
 package samsamoo.ai_mockly.domain.feedback.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import samsamoo.ai_mockly.domain.feedback.domain.Feedback;
 import samsamoo.ai_mockly.domain.member.domain.Member;
 
@@ -12,5 +14,9 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
     List<Feedback> findAllByMember(Member member);
 
-    Optional<Feedback> findByMemberAndCreatedAt(Member member, LocalDateTime createdAt);
+    @Query("SELECT f FROM Feedback f WHERE f.member = :member AND f.createdAt BETWEEN :startTime AND :endTime " +
+            "ORDER BY f.createdAt DESC LIMIT 1")
+    Optional<Feedback> findByMemberAndCreatedAtBetweenOrderByCreatedAtDesc(@Param("member") Member member,
+                                                                           @Param("startTime") LocalDateTime startTime,
+                                                                           @Param("endTime") LocalDateTime endTime);
 }
